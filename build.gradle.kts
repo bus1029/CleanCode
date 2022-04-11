828931cdb65a9b95c10f8bcb6f4901c9d967865e
Seokhyun Bae <bus1029@gmail.com>
2022-04-11 12:15:59 +0900

    feat: boolean만 지원하는 명령행 인수 구문분석기 개발
    - 명령행인수 구문분석기의 최초 버전인 boolean만 지원하는 구문분석기 
    작성 (한 글자의 명령만 지원, -f, -e, -ef, ...)
    - 테스트코드가 없었기 때문에 구문분석기 학습 테스트 작성

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,8 @@ repositories {
 }
 
 dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
+    testImplementation("org.assertj:assertj-core:3.22.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
 }
 
---
+++ src/test/java/parser/ArgsTest.java
@@ -0,0 +1,54 @@
+package parser;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ArgsTest {
+  @Test
+  void testBooleanArgs_success() {
+    String[] args = {"-t", "-f", "-a", "-s"};
+    Args parser = new Args("t,f,a,s", args);
+
+    assertThat(parser.isValid()).isTrue();
+    assertThat(parser.cardinality()).isEqualTo(4);
+    assertThat(parser.getBoolean('t')).isTrue();
+    assertThat(parser.getBoolean('f')).isTrue();
+    assertThat(parser.getBoolean('a')).isTrue();
+    assertThat(parser.getBoolean('s')).isTrue();
+  }
+
+  @Test
+  void testBooleanArgs_failForEmptyArgs() {
+    String[] args = {};
+    Args parser = new Args("t,f,a,s", args);
+
+    assertThat(parser.isValid()).isTrue();
+    assertThat(parser.cardinality()).isZero();
+    assertThat(parser.getBoolean('t')).isFalse();
+    assertThat(parser.getBoolean('f')).isFalse();
+    assertThat(parser.getBoolean('a')).isFalse();
+    assertThat(parser.getBoolean('s')).isFalse();
+  }
+
+  @Test
+  void testBooleanArgs_partialSuccess() {
+    String[] args = {"-t", "-as", "#"};
+    Args parser = new Args("t,a,#,$", args);
+
+    assertThat(parser.isValid()).isFalse();
+    System.out.println(parser.errorMessage());
+    assertThat(parser.cardinality()).isEqualTo(2);
+    assertThat(parser.getBoolean('t')).isTrue();
+    assertThat(parser.getBoolean('a')).isTrue();
+  }
+
+  @Test
+  void testBooleanArgs_wrongFormatSchema() {
+    String[] args = {"-t", "-f", "-a", "-s"};
+    Args parser = new Args("tf,-a, -s", args);
+
+    assertThat(parser.isValid()).isTrue();
+    assertThat(parser.cardinality()).isZero();
+  }
+}
